dc8233fb2f5ef6302b5af1d2dbe6380e04a224c7
Sayed <mdsayedgv@gmail.com>
2022-05-26 09:53:41 +0600

    add delete user API

--- index.js
+++ index.js
@@ -194,7 +194,12 @@ async function run() {
     });
 
     // Call from My Order Page
-    
+    app.delete("/order/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.send(result);
+    });
 
 
     app.get("/review", async (req, res) => {
